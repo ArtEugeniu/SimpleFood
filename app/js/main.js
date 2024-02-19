6a486587394ab1dbf2105be971cd011f01ff16c4
ArtEugeniu <artemieff96@gmail.com>
2024-02-19 10:44:39 +0100

    Задание 12 header + category + footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,8 @@
 $ (function(){
- 
+  $('.menu__link, .footer__link').on('click', function () {
+    $('.menu__link, .footer__link').removeClass('menu__link--active');
+    $(this).addClass('menu__link--active')
+  });
 });
+
+var mixer = mixitup('.category__content');
