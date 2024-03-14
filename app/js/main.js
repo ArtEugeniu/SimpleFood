de5db330436f92538079f3c44eea6f8f4bc5fa60
ArtEugeniu <artemieff96@gmail.com>
2024-03-14 23:04:42 +0100

    'Третья страница'

--- app/js/main.js
+++ app/js/main.js
@@ -9,7 +9,7 @@ $(function () {
     $(this).addClass('pagination__link--active')
   });
 
-  $('.product-catalog__select').styler();
+  $('.product-catalog__select, .product__num').styler();
 
   $('.price__input').ionRangeSlider({
     type: 'double',
@@ -20,7 +20,27 @@ $(function () {
     onChange: function (data) {
       $('.price__from').text(data.from);
       $('.price__to').text(data.to);
-  }
+    }
+  });
+
+  $('.product-tabs__top-item').on('click', function (e) {
+    e.preventDefault();
+    $('.product-tabs__top-item').removeClass('product-tabs__top-item--active');
+    $(this).addClass('product-tabs__top-item--active')
+
+    $('.product-tabs__content-item').removeClass('product-tabs__content-item--active');
+    $($(this).attr('href')).addClass('product-tabs__content-item--active');
+  });
+
+  $('.product__slider').slick({
+    prevArrow: '<button type="button" class="product__slider-arrow product__slider-arrow--prev">\
+    <svg width="19.000000" height="31.000000" viewBox="0 0 19 31" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">\
+	<path id="Vector" d="M0.38 14.22L16.39 -0.17C16.92 -0.68 17.77 -0.68 18.3 -0.17C18.82 0.35 18.82 1.19 18.3 1.7L3.25 15.22L18.29 28.74C18.82 29.26 18.82 30.09 18.29 30.61C17.77 31.12 16.92 31.12 16.39 30.61L0.38 16.22C0.1 15.95 -0.02 15.58 0 15.22C-0.02 14.86 0.1 14.5 0.38 14.22Z" fill="#363853" fill-opacity="1.000000" fill-rule="nonzero"/>\
+</svg></button>',
+    nextArrow: '<button type="button" class="product__slider-arrow product__slider-arrow--next">\
+<svg width="19.000000" height="31.000000" viewBox="0 0 19 31" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">\
+<path id="Vector" d="M0.38 14.22L16.39 -0.17C16.92 -0.68 17.77 -0.68 18.3 -0.17C18.82 0.35 18.82 1.19 18.3 1.7L3.25 15.22L18.29 28.74C18.82 29.26 18.82 30.09 18.29 30.61C17.77 31.12 16.92 31.12 16.39 30.61L0.38 16.22C0.1 15.95 -0.02 15.58 0 15.22C-0.02 14.86 0.1 14.5 0.38 14.22Z" fill="#363853" fill-opacity="1.000000" fill-rule="nonzero"/>\
+</svg></button>'
   });
 
   $('.reviews__slider').slick({
@@ -39,14 +59,14 @@ $(function () {
       {
         breakpoint: 560,
         settings: {
-          dots:false
+          dots: false
         },
       },
     ],
 
   });
 
-  $(window).on('load resize', function() {
+  $(window).on('load resize', function () {
     if ($(window).width() < 560) {
       $('.resto__items:not(.slick-initialized)').slick({
         arrows: false,
@@ -59,7 +79,7 @@ $(function () {
     }
   });
 
-  $(window).on('load resize', function() {
+  $(window).on('load resize', function () {
     if ($(window).width() < 768) {
       $('.resto__items:not(.slick-initialized)').slick({
         arrows: false,
@@ -72,15 +92,15 @@ $(function () {
     }
   });
 
-  $(window).on('load resize', function() {
+  $(window).on('load resize', function () {
     if ($(window).width() < 560) {
       $('.reviews__slider').slick({
         dots: false
       });
-    } 
+    }
   });
 
-  $(window).on('load resize', function() {
+  $(window).on('load resize', function () {
     if ($(window).width() < 768) {
       $('.discounts__list:not(.slick-initialized)').slick({
         arrows: false,
@@ -91,8 +111,88 @@ $(function () {
     } else {
       $(".discounts__list.slick-initialized").slick("unslick");
     }
-  }); 
- 
+  });
+
+  
+
+  $(window).on('load resize', function () {
+    if ($(window).width() < 560) {
+      $('.suggestion__list:not(.slick-initialized)').slick({
+        arrows: false,
+        dots: true,
+        infinite: true,
+        slidesToShow: 2
+      });
+    } else {
+      $(".suggestion__list.slick-initialized").slick("unslick");
+    }
+  });
+
+  $(window).on('load resize', function () {
+    if ($(window).width() < 768) {
+      $('.suggestion__list:not(.slick-initialized)').slick({
+        arrows: false,
+        dots: true,
+        infinite: true,
+        slidesToShow: 3
+      });
+    } else {
+      $(".suggestion__list.slick-initialized").slick("unslick");
+    }
+  });
+
+  $(window).on('load resize', function () {
+    if ($(window).width() < 992) {
+      $('.suggestion__list:not(.slick-initialized)').slick({
+        arrows: false,
+        dots: true,
+        infinite: true,
+        slidesToShow: 4
+      });
+    } else {
+      $(".suggestion__list.slick-initialized").slick("unslick");
+    }
+  });
+
+  $(".product__rating, .comments__rating").rateYo({
+    normalFill: "#c1c1c1",
+    spacing: '6px',
+    starWidth: '16px',
+    ratedFill: '#FFB800',
+    readOnly: true,
+    starSvg: '<svg width="16.000000" height="16.000000" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">\
+    <defs>\
+      <clipPath id="clip3197_367">\
+        <rect id="star 4" width="16.000000" height="16.000000" transform="matrix(-1 0 0 1 16 0)" fill="white" fill-opacity="0"/>\
+      </clipPath>\
+    </defs>\
+    <rect id="star 4" width="16.000000" height="16.000000" transform="matrix(-1 0 0 1 16 0)" fill="#FFFFFF" fill-opacity="0"/>\
+    <g clip-path="url(#clip3197_367)">\
+      <path id="Vector" d="M0.02 6.16C0.07 5.99 0.22 5.87 0.4 5.84L5.36 5.12L7.57 0.62C7.65 0.46 7.82 0.36 7.99 0.36C8.17 0.36 8.34 0.46 8.42 0.62L10.63 5.12L15.59 5.84C15.77 5.87 15.92 5.99 15.97 6.16C16.03 6.33 15.98 6.52 15.85 6.64L12.26 10.14L13.11 15.08C13.14 15.25 13.07 15.43 12.93 15.54C12.84 15.6 12.75 15.63 12.65 15.63C12.57 15.63 12.5 15.61 12.43 15.57L8 13.24L3.56 15.57C3.4 15.66 3.21 15.64 3.07 15.54C2.92 15.43 2.85 15.25 2.88 15.08L3.73 10.14L0.14 6.64C0.01 6.52 -0.04 6.33 0.02 6.16Z" fill-rule="nonzero"/>\
+    </g>\
+  </svg>',
+  });
+
+  $(".comment-form__star").rateYo({
+    normalFill: "#c1c1c1",
+    spacing: '6px',
+    starWidth: '16px',
+    ratedFill: '#FFB800',
+    starSvg: '<svg width="16.000000" height="16.000000" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">\
+    <defs>\
+      <clipPath id="clip3197_367">\
+        <rect id="star 4" width="16.000000" height="16.000000" transform="matrix(-1 0 0 1 16 0)" fill="white" fill-opacity="0"/>\
+      </clipPath>\
+    </defs>\
+    <rect id="star 4" width="16.000000" height="16.000000" transform="matrix(-1 0 0 1 16 0)" fill="#FFFFFF" fill-opacity="0"/>\
+    <g clip-path="url(#clip3197_367)">\
+      <path id="Vector" d="M0.02 6.16C0.07 5.99 0.22 5.87 0.4 5.84L5.36 5.12L7.57 0.62C7.65 0.46 7.82 0.36 7.99 0.36C8.17 0.36 8.34 0.46 8.42 0.62L10.63 5.12L15.59 5.84C15.77 5.87 15.92 5.99 15.97 6.16C16.03 6.33 15.98 6.52 15.85 6.64L12.26 10.14L13.11 15.08C13.14 15.25 13.07 15.43 12.93 15.54C12.84 15.6 12.75 15.63 12.65 15.63C12.57 15.63 12.5 15.61 12.43 15.57L8 13.24L3.56 15.57C3.4 15.66 3.21 15.64 3.07 15.54C2.92 15.43 2.85 15.25 2.88 15.08L3.73 10.14L0.14 6.64C0.01 6.52 -0.04 6.33 0.02 6.16Z" fill-rule="nonzero"/>\
+    </g>\
+  </svg>',
+  });
+
+  $('.popup-link').magnificPopup();
+
 });
 
 
@@ -101,9 +201,9 @@ $(function () {
 
 document.addEventListener('DOMContentLoaded', () => {
 
-  const burger = document.querySelector('.burger'); 
-  const mobileMenu = document.querySelector('.mobile-menu'); 
-  const bodyLock = document.querySelector('body'); 
+  const burger = document.querySelector('.burger');
+  const mobileMenu = document.querySelector('.mobile-menu');
+  const bodyLock = document.querySelector('body');
   const closeButton = document.querySelector('.mobile-menu__button')
 
   burger.addEventListener('click', () => {
@@ -138,9 +238,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
 document.addEventListener('DOMContentLoaded', () => {
 
-  const openButton = document.querySelector('.product-catalog__open-btn'); 
-  const mobileFilter = document.querySelector('.product-catalog__filters'); 
-  const bodyLock = document.querySelector('body'); 
+  const openButton = document.querySelector('.product-catalog__open-btn');
+  const mobileFilter = document.querySelector('.product-catalog__filters');
+  const bodyLock = document.querySelector('body');
   const closeButton = document.querySelector('.product-catalog__close-btn');
 
   openButton.addEventListener('click', () => {
@@ -171,7 +271,7 @@ document.addEventListener('DOMContentLoaded', () => {
       bodyLock.classList.remove('lock');
     }
   });
-  
+
 });
 
 
